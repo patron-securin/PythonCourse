f3946c426d214988d489bb9fc438b5ce26e3f2a3
patron-securin <47637153+patron-securin@users.noreply.github.com>
2019-04-25 20:06:08 +0300

    Update hw_C4.py

--- hw_C4.py
+++ hw_C4.py
@@ -1,6 +1,6 @@
 from math import sqrt
 def quadratic_equation(a, b, c):
-  D=b^2-4*a*c
+  D=b**2-4*a*c
   if D<0:
     print('Корней нет. P.s: они есть, но по условию просят так написать')
   elif D>=0:
